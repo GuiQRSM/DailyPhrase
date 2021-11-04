2442fbb61734682f896540859dbc7f4820b5bac3
GuiQRSM <endbeob15@gmail.com>
2021-11-04 17:04:29 -0300

    Use the set-upstream arg:

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
 
   runApp(MaterialApp(
     title: "Daily Phrase",
-    home: Column(children: const <Widget>[
-      Text("Co1"),
-      Text("Co2"),
-      Text("Co3"),
-    ],),
-    //home: Container(color: Colors.white,),
+    home: Container(
+      color: Colors.white,
+      child: Column(children: const <Widget>[
+      Text(
+          "Lorem ipsum",
+           style: TextStyle(
+             fontSize: 35,
+             fontStyle: FontStyle.italic,
+             letterSpacing: 1,
+             wordSpacing: 0,
+             decoration: TextDecoration.underline,
+             fontWeight: FontWeight.w700,
+             color: Colors.blueGrey,
+           ),
+      ),
+      ],
+      ),
+    ),
   ));
 
 }
-
