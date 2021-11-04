cf1db8543fb4db1f44df21f1dcef92d1ba3b252d
GuiQRSM <endbeob15@gmail.com>
2021-11-04 17:45:00 -0300

    implementation of a button.

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,27 @@ void main() {
     title: "Daily Phrase",
     home: Container(
       color: Colors.white,
-      child: Column(children: const <Widget>[
-        Text(
-            "Loftus Check!",
-        style: TextStyle(
-          fontSize: 30,
-          fontStyle: FontStyle.italic,
-          color: Colors.deepOrange,
-          fontWeight: FontWeight.w600,
-          decoration: TextDecoration.underline,
-          decorationStyle: TextDecorationStyle.wavy,
-          decorationColor: Colors.redAccent,
-          letterSpacing: 3,
-        ),
-        ),
-      ],),
+      child: Column(children: <Widget>[
+        FlatButton(
+            onPressed: (){
+              print("Pressionado!");
+            },
+            child: Text(
+                "Clique aqui!",
+              style: TextStyle(
+                fontSize: 35,
+                color: Colors.teal,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+                letterSpacing: 4,
+                decoration: TextDecoration.underline,
+                decorationStyle: TextDecorationStyle.wavy,
+                decorationColor: Colors.tealAccent,
+              ),
+            ),
+        )
+      ],
+      ),
     ),
   ));
 
