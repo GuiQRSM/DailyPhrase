dbed19a2f6e8aba12de27e8466b773bbbbd5b816
GuiQRSM <endbeob15@gmail.com>
2021-11-06 17:33:38 -0300

    Implements border, padding and margin in the interface structure container.

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,23 @@ void main() {
     title: "Daily Phrase",
     home: Container(
       //color: Colors.white,
+      padding: EdgeInsets.only(top: 40, bottom: 40),
+      margin: EdgeInsets.fromLTRB(30, 40, 30, 40),
       decoration: BoxDecoration(
-        border: Border.all(width: 5, color: Colors.indigo),
-      ),
+       border: Border.all(width: 5, color: Colors.lightBlue)
+     ),
+
       child: Column(children: const <Widget>[
         Text(
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut scelerisque nisl.",
           textAlign: TextAlign.justify,
           style: TextStyle(
             fontSize: 38,
+            color: Colors.indigo,
+            decorationColor: Colors.lightBlueAccent
           ),
             ),
-      ],
+              ],
       ),
     ),
   ));
