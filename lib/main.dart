f7ae697080d47ab8f0fc5c871582508249307c14
GuiQRSM <endbeob15@gmail.com>
2021-11-10 15:38:12 -0300

    Change background image

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,25 @@ void main() {
     debugShowCheckedModeBanner: false,
     title: "Daily Phrase",
     home: Container(
-      margin: const EdgeInsets.only(top: 40),
-      decoration: BoxDecoration(
-       border: Border.all(width: 5, color: Colors.lightBlue)
+     margin: const EdgeInsets.only(top: 41),
+     decoration: BoxDecoration(
+       border: Border.all(width: 6, color: Colors.deepOrange)
      ),
-      child: Image.asset(
-        "images/purplerain.jpg",
+     child: Image.asset(
+       "images/blackorange.jpg",
         fit: BoxFit.cover,
-      ),
+     ),
+
+      /*child: Row(
+        children: const <Widget>[
+        Text("T1",),
+          Text("T2"),
+           Text("T3"),
+        ],
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+      ),*/
+
     ),
   ));
 
