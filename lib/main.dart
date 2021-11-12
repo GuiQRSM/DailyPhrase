8cf4cac776a41004b4bed7468244372819f62f10
GuiQRSM <endbeob15@gmail.com>
2021-11-12 14:21:14 -0300

    Redefines the base layout structure, again using the Scaffold() class.

--- lib/main.dart
+++ lib/main.dart
@@ -9,40 +9,72 @@ void main() {
     home: Scaffold(
       appBar: AppBar(
         title: Text("Daily Phrase"),
-        backgroundColor: Colors.deepOrange,
+        backgroundColor: Colors.orange,
       ),
       body: Padding(
         padding: EdgeInsets.all(16),
         child: Text(
-            "Test for body!",
-            style: TextStyle(
-            fontSize: 30,
-              color: Colors.deepOrangeAccent,
-        ),
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.deepOrange,
+            "Test for Body",
+        style: TextStyle(
+          color: Colors.orangeAccent,
+          fontSize: 17,
+          fontWeight: FontWeight.w600,
+          letterSpacing: 3,
+          fontStyle: FontStyle.italic,
+           ),
+         ),
+       ),
+      bottomNavigationBar:BottomAppBar(
+        color: Colors.orange,
         child: Padding(
           padding: EdgeInsets.all(16),
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              Text("botton1",
-            style: TextStyle(
-              fontSize: 15,
-              color: Colors.white,
-            ),
-          ),
-          Text("botto2",
-            style: TextStyle(
-              fontSize: 15,
-              color: Colors.white,
-            ),
-          ),
+              Text(
+                  "bt1",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+              Text(
+                  "bt2",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+              Text(
+                  "bt3",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+              Text(
+                  "bt4",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
             ],
           ),
         ),
-      ),
+      ) ,
     ),
   ));
 
