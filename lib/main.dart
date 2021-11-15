7d53bbde99360e76d1e3adf5bbfa21f64835e764
GuiQRSM <endbeob15@gmail.com>
2021-11-15 14:52:24 -0300

    Implements the logoOrange.png image file, as the central logo of the application's interface.

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class HomeStateFul extends StatefulWidget {
   _HomeStateFulState createState() => _HomeStateFulState();
 }
 
-var _text = "Teste Phrase";
+var _text = "";
 
 class _HomeStateFulState extends State<HomeStateFul> {
   @override
@@ -35,15 +35,10 @@ class _HomeStateFulState extends State<HomeStateFul> {
       body: Column(
         children: [
           Padding(
-            padding: EdgeInsets.fromLTRB(120, 190,120, 190),
-            child: Text(
-              "Frase do dia!",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
-                color: Colors.orange,
-              ),
-            ),
+            padding: EdgeInsets.fromLTRB(120, 170,120, 170),
+            child: Image.asset(
+              "images/logoOrange.png",
+            )
           ),
           Text(
             "$_text",
